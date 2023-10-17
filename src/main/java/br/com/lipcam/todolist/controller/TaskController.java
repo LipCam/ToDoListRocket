d7ab2c368289417924f3ef09e18470bc5acda405
LipCam <lipcam@hotmail.com>
2023-10-17 13:33:01 -0300

    Get Tasks by IdUser

--- src/main/java/br/com/lipcam/todolist/controller/TaskController.java
+++ src/main/java/br/com/lipcam/todolist/controller/TaskController.java
@@ -6,10 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -35,5 +32,10 @@ public class TaskController {
         //recuperando o atributo do filter e setando no IdUser do Task
         entity.setIdUser((UUID)request.getAttribute("idUser"));
         return ResponseEntity.status(HttpStatus.CREATED).body(iTaskRepository.save(entity));
+    }
+
+    @GetMapping
+    public ResponseEntity getAllByIdUser(HttpServletRequest request){
+        return ResponseEntity.status(HttpStatus.OK).body(iTaskRepository.findByIdUser((UUID)request.getAttribute("idUser")));
     }
 }
--- src/main/java/br/com/lipcam/todolist/repository/ITaskRepository.java
+++ src/main/java/br/com/lipcam/todolist/repository/ITaskRepository.java
@@ -3,8 +3,9 @@ package br.com.lipcam.todolist.repository;
 import br.com.lipcam.todolist.model.TaskModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
-
+    List<TaskModel> findByIdUser(UUID idUser);
 }
